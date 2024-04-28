de6e3609287ec5ab2024138d03b466633b49f621
Palin Wiseman <WISEPB1@student.op.ac.nz>
2024-04-29 09:41:26 +1200

    Added error checking to printing

--- OpenF1Api.py
+++ OpenF1Api.py
@@ -1,7 +1,35 @@
-import requests
-BASE_URL = 'https://api.openf1.org/v1/'
+# When on a new computer: Run python -m pip install requests
+# If this doesn't work check https://pypi.org/project/requests/
 # payload = {'key1': 'value1', 'key2': ['value2', 'value3']} Can use a list of items as a value
 # https://openf1.org/ for more information on the API
+# 1	    Max Verstappen
+# 2	    Logan Sargeant
+# 3	    Daniel Ricciardo
+# 4	    Lando Norris
+# 10	Pierre Gasly
+# 11	Sergio Perez
+# 14	Fernando Alonso
+# 16	Charles Leclerc
+# 18	Lance Stroll
+# 20	Kevin Magnussen
+# 22	Yuki Tsunoda
+# 23	Alex Albon
+# 24	Zhou Guanyu
+# 27	Nico Hulkenberg
+# 31	Esteban Ocon
+# 44	Lewis Hamilton
+# 55	Carlos Sainz Jr
+# 63	George Russell
+# 77	Valtteri Bottas
+# 81	Oscar Piastri
+# Driver numbers for testing
+# Multiviewer API link https://mvf1.readthedocs.io/en/latest/MultiViewerForF1.html
+# pip install mvf1
+# https://github.com/RobSpectre/mvf1/issues/2
+# https://github.com/RobSpectre/mvf1
+import requests
+BASE_URL = 'https://api.openf1.org/v1/'
+
 dict = {}
 dict['ca'] = 'car_data'
 dict['dr'] = 'drivers'
@@ -19,21 +47,36 @@ dict['we'] = 'weather'
 
 def main():
     payload = {'meeting_key' : 'latest', 'session_type': 'Qualifying'}
-    r = get_request(dict['se'], payload)
-    session_key = r[-1]['session_key']
-    payload = {'meeting_key' : 'latest', 'session_key': session_key, 'driver_number' : 1, 'is_pit_out_lap' : 'false'}
+    quals = get_request(dict['se'], payload)
+    session_key = quals[-1]['session_key']
+    payload = {'meeting_key' : 'latest', 'session_key': session_key, 'driver_number' : 16}
     r = get_request(dict['la'], payload)
-    latest_lap = r[-5] #to -2 for testing as -1 is giving me crap
-    print(latest_lap)
-    sector_print(latest_lap['segments_sector_1'], 1)
-    sector_print(latest_lap['segments_sector_2'], 2)
-    sector_print(latest_lap['segments_sector_3'], 3)
+    try:
+        for lap in r:
+            print('\n')
+            if lap['is_pit_out_lap'] == 1:
+                print('Pit out lap')
+            elif not lap['segments_sector_3']: #If the last sector is empty it is a pit in lap
+                print('Pit in lap')
+            else:
+                sector_print(lap['segments_sector_1'], 1)
+                print('Time:', lap['duration_sector_1'])
+                sector_print(lap['segments_sector_2'], 2)
+                print('Time:', lap['duration_sector_2'])
+                sector_print(lap['segments_sector_3'], 3)
+                print('Time:', lap['duration_sector_3'])
+                print('Lap time:', lap['lap_duration'])
+    except:
+        print('Error')
 
 #Function to loop through a sector
 def sector_print(sector, sector_number):
     print ('Sector', sector_number, ':')
+    s = ''
     for i in sector:
-        print(section_code(i))
+        s += section_code(i) + ', '
+    s = s.rstrip(', ')
+    print(s)
 
 
 
@@ -41,15 +84,15 @@ def sector_print(sector, sector_number):
 def section_code(code):
     match code:
         case 2048:
-            return 'Yellow'
+            return 'Y'
         case 2049:
-            return 'Green'
+            return 'G'
         case 2051:
-            return 'Purple'
+            return 'P'
         case 0:
-            return 'Not Available'
+            return 'N/A'
         case _:
-            return 'Unknown: ' + str(code)
+            return 'N/A'
 
 # Function to get request from the API
 def get_request(endpoint, payload, time = 10):
