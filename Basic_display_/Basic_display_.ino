a1dac3da1d67e111d28fc1a6017c22b07f6b9593
Palin Wiseman <palinbay@hotmail.com>
2024-06-14 16:48:04 +1200

    Changed display to work with 2 screens

--- Basic_display_/Basic_display_.ino
+++ Basic_display_/Basic_display_.ino
@@ -3,48 +3,49 @@
 
 // Set the LCD address to 0x3F for a 16 chars and 2 line display
 LiquidCrystal_I2C lcd(0x27, 16, 2);
+LiquidCrystal_I2C lcd2(0x26, 16, 2);
 
 class Driver {
   public:
-  String code;
-  String firstName;
-  String lastName;
+  String tla;
   String number;
-  String team;
   String teamCode;
 };
 
-Driver drivers[20];
+Driver drivers[8];
+int adjustedNum;
 
 void setup()
 {
-  drivers[0] = {"VER", "Max", "Verstappen", "1", "Red Bull", "RBR "};
-  drivers[1] = {"NOR", "Lando", "Norris", "4", "McLaren", "MCL "};
-  drivers[2] = {"HAM", "Lewis", "Hamilton", "44", "Mercedes", "MERC"};
+  drivers[0] = {"VER", "1", "RBR "};
+  drivers[1] = {"GAS", "10", "ALP "};
+  drivers[2] = {"NOR", "4", "MCL "};
+  drivers[3] = {"PIA", "81", "MCL "};
   lcd.begin();
   lcd.backlight();
+  lcd.clear();
+  lcd2.clear();
+  lcd.setCursor(0,0);
+  printDriver(1, drivers[0]);
+  lcd.setCursor(0,1);
+  printDriver(2, drivers[1]);
+  lcd2.setCursor(0,0);
+  printDriverScreen2(3, drivers[2]);
+  lcd2.setCursor(0,1);
+  printDriverScreen2(4, drivers[3]);
+  delay(2000);
 }
 
 void loop()
 {
-  lcd.setCursor(0,0);
-  printDriver("1", drivers[0]);
-  lcd.setCursor(0,1);
-  printDriver("2", drivers[1]);
-  delay(4000);
-  lcd.setCursor(0,0);
-  printDriver("2", drivers[1]);
-  lcd.setCursor(0,1);
-  printDriver("3", drivers[2]);
-  delay(4000);
-  lcd.setCursor(0,0);
-  printDriver("3", drivers[2]);
-  lcd.setCursor(0,1);
-  printDriver("1", drivers[0]);
-  delay(4000);
+
 }
 
-void printDriver(String place, Driver driver)
+void printDriver(int place, Driver driver)
 {
-  lcd.print("P" + place + " - " + driver.teamCode + " - " + driver.code);
+  lcd.print("P" + String(place) + " - " + driver.teamCode + " - " + driver.tla);
+}
+void printDriverScreen2(int place, Driver driver)
+{
+  lcd2.print("P" + String(place) + " - " + driver.teamCode + " - " + driver.tla);
 }
