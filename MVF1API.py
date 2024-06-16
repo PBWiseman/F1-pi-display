67cf72131ef62e391b444168a0458430ad6d145c
PBWiseman <wisepb1@student.op.ac.nz>
2024-06-16 16:21:01 +1200

    Fixed error with manually closing player

--- MVF1API.py
+++ MVF1API.py
@@ -2,27 +2,6 @@
 # If not try Run python -m pip install requests
 # If this doesn't work check https://pypi.org/project/requests/
 # payload = {'key1': 'value1', 'key2': ['value2', 'value3']} Can use a list of items as a value
-# 1	    Max Verstappen
-# 2	    Logan Sargeant
-# 3	    Daniel Ricciardo
-# 4	    Lando Norris
-# 10	Pierre Gasly
-# 11	Sergio Perez
-# 14	Fernando Alonso
-# 16	Charles Leclerc
-# 18	Lance Stroll
-# 20	Kevin Magnussen
-# 22	Yuki Tsunoda
-# 23	Alex Albon
-# 24	Zhou Guanyu
-# 27	Nico Hulkenberg
-# 31	Esteban Ocon
-# 44	Lewis Hamilton
-# 55	Carlos Sainz Jr
-# 63	George Russell
-# 77	Valtteri Bottas
-# 81	Oscar Piastri
-# Driver numbers for testing
 # Multiviewer API link https://mvf1.readthedocs.io/en/latest/MultiViewerForF1.html
 # pip install mvf1
 # https://github.com/RobSpectre/mvf1/issues/2
@@ -37,24 +16,44 @@ from mvf1 import MultiViewerForF1
 mvf1 = MultiViewerForF1()
 createdPlayerID = None
 createdPlayer = None
-players = mvf1.players
-contentId = players[0].content_id
 
 def openWindow(driverNumber):
+    players = mvf1.players
+    contentId = players[0].content_id
+    global createdPlayer
+    global createdPlayerID
     #Take the driverNumber and open the window for that driver
     try:
-        #If the player is already created and is the same driver then do nothing
-        if createdPlayer == None:
-            createdPlayerID = mvf1.player_create(content_id = contentId, driver_number = driverNumber)
-            time.sleep(1)
-            mvf1.player_set_fullscreen(createdPlayerID, True)
-            mvf1.player_sync_to_commentary(createdPlayerID, True)
+        if createdPlayer is not None:
+            try:
+                # Try to access a property of the player to see if it's still open
+                _ = createdPlayer.driver_data
+            except Exception:
+                # If accessing the property failed, the window was probably closed
+                createdPlayer = None
+                createdPlayerID = None
+            else:
+                if createdPlayer.driver_data["driverNumber"] == driverNumber:
+                    return True
+                else:
+                    createdPlayer.delete()
+                    createdPlayer = None
+                    createdPlayerID = None
+        response = mvf1.player_create(content_id = contentId, driver_number = driverNumber, fullscreen = True, always_on_top = True)
+        createdPlayerID = response['data']['playerCreate']
+        retries = 10
+        interval = .5
+        for _ in range(retries):
             createdPlayer = mvf1.player(createdPlayerID)
-        elif createdPlayer.driver_data["driverNumber"] == driverNumber:
-            return True
-        else:
-            createdPlayer.switch_stream(drivers.getDriverTLA(driverNumber))
+            if createdPlayer:
+                break
+            time.sleep(interval)
+        if not createdPlayer:
+            raise Exception("Player creation failed after retries.")
+        createdPlayer.set_fullscreen(True)
+        mvf1.player_sync_to_commentary()
         return True
     except Exception as e:
         print(str(e))
         return False
+    
